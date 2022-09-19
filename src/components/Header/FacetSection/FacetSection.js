58882ead7625c928d5303bd3178227c5be08db7a
vincentluciani <vincent.luciani.fr@gmail.com>
2022-09-19 15:14:52 +0200

    Initial commit

---
+++ src/components/Header/FacetSection/FacetSection.js
@@ -0,0 +1,14 @@
+import './FacetSection.css';
+import FacetSublist from './FacetSublist.js.js'
+
+const FacetSection= props =>{
+  return(
+  <div class="filters">
+      <div class="category-label">Category</div>
+     {props.facetData.map(item => <div>{item.key}<FacetSublist facetList={item.subCategory.buckets} category={item.key} term={props.term}/></div>)}
+     <br /><a href={"?term"+props.term} ><div>&lt; All Categories</div></a>
+  </div>
+);
+}
+export default FacetSection;
+
