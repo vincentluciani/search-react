41aa725357337ef5e42aa82ae95fa536acb0db1c
vincent luciani <vincent.luciani.fr@gmail.com>
2022-10-01 17:27:48 +0200

    small design corrections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,12 @@ const Header= props =>{
   return(
     <div className="result-header-wrapper" role="header">
     { !isNaN(props.totalHits) && (props.totalHits == 0) && <div className="result-header">No result found for {props.term}</div> }
-    { !isNaN(props.totalHits) && (props.totalHits > 0) && <div className="result-header">Showing results {props.start}-{props.end} from {props.totalHits} results for {props.term}</div> }
+    { !isNaN(props.totalHits) && (props.totalHits > 0) && 
+    <div className="result-header">
+      <div class="result-summary">Showing results {props.start}-{props.end} from {props.totalHits} results for {props.term}</div>
+      <div class="result-actions">Table Boxes</div>
+    </div>
+    }
     </div>
 );
 }
