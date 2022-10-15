a2918b9efd9e2272c9d914e474701269b612612a
vincentluciani <vincent.luciani.fr@gmail.com>
2022-10-15 15:41:24 +0200

    take off application style

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,15 @@ var replace = require('gulp-replace');
 var fs = require('fs');
 
 gulp.task('move-js', function () {    
-    return gulp.src(['build/static/js/main.da6b2933.js'])
+    return gulp.src(['build/static/js/main.b9c9aa11.js'])
         .pipe(gulp.dest('output'));
 });
 
 gulp.task('process-html', function () {    
     return gulp.src(['build/index.html'])
-        .pipe(replace('<link href="/static/css/main.9f303c6e.css" rel="stylesheet">', '<style>'+fs.readFileSync('build/static/css/main.9f303c6e.css', 'utf8')+'</style>'))  
-        .pipe(replace('<script defer="defer" src="/static/js/main.da6b2933.js"></script>','<script defer="defer" src="main.da6b2933.js"></script>'))
+        .pipe(replace('<link href="/static/css/main.fdda3242.css" rel="stylesheet">', '<style>'+fs.readFileSync('build/static/css/main.fdda3242.css', 'utf8')+'</style>'))  
+        .pipe(replace('<script defer="defer" src="/static/js/main.b9c9aa11.js"></script>','<script defer="defer" src="main.b9c9aa11.js"></script>'))
+        .pipe(replace('font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;',''))
         .pipe(gulp.dest('output'));
 });
 
